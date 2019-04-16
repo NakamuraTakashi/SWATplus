d75df4b96bb052e71edf0b27835bb544b1fdc528
AGNET\nancy.sammons <nancy.sammons@ars.usda.gov>
2019-04-16 08:30:20 -0500

    Version 59
    Summary of changes:
    -- Added FLO_IN and AQU_IN to the aquifer_sd outputs
    --Units added back to ALL output files
    --LUM-DTL - removed some old plants that were renamed in plants.plt
    --IRR.OPS - updated file to new format
    --CAL_PARMS.CAl - added new calibration variables
    --PESTICIDE.PST - 2 headers changed
    --PRINT.PRT - those commit datasets that contained HRU_CS were removed;
    -- RES_REL.DTL - prinipal mispelling changed to principle
    --RECCNST added in EXCO
    --updated recall input files deleting some old vars
    --an irrigation issue fixed in this revision
    --FILE.CIO - in_chg files renamed and reorderd;
    --new subroutines added
    -- EOF added to many read routines;

--- data/Little_River_Tifton/nutrients.sol
+++ data/Little_River_Tifton/nutrients.sol
@@ -1,3 +1,3 @@
 nutrients.sol: Soil nutrients - LREW
-id   name                     dp_co         tot_n         min_n         org_n         tot_p         min_p         org_p         sol_p         h3a_p        mehl_p        bray_p  
-1    soilnut1              13.00000       6.00000       3.00000       3.00000       3.50000       0.40000       0.15000       0.25000       1.20000       0.85000       0.85000  
+name                     dp_co         tot_n         min_n         org_n         tot_p         min_p         org_p         sol_p         h3a_p        mehl_p        bray_p  
+soilnut1              13.00000       6.00000       3.00000       3.00000       3.50000       0.40000       0.15000       0.25000       1.20000       0.85000       0.85000  
